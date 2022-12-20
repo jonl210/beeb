b7dde5d15c8da8b3d9726d90a295a6bb58617b6f
Jonathan Lambert <jonathanlambert247@gmail.com>
2022-12-20 07:12:24 -0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { View, Text } from "react-native";
+
+const Drawer = createDrawerNavigator();
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
+};
+
+const TestDrawer = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen component={MainScreen} />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="inverted" />
+      <TestDrawer />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
